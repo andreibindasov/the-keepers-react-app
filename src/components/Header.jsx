5634ece3d051401a44ce4b2f1bd8c2b9d3bc29fa
Andrei <bindasov.andrei@gmail.com>
2023-12-14 16:32:40 -0700

    adding masonic keys

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
+
 export default class Header extends Component {
   render() {
     return (
       <header>
             The Keepers
+            <div className="float-right">
+              <div className='masonicKey26'></div>
+              <div className='masonicKey13'></div>
+              <div className='masonicKey21'></div>
+              <div className='masonicKey5'></div>
+              &nbsp;
+              <div className='masonicKey3'></div>
+              <div className='masonicKey11'></div>
+              <div className='masonicKey9'></div>
+              <div className='masonicKey22 key22'></div>
+            </div>
+            
       </header>
     )
   }
